022de5f37bcaabb5db37b409b3c8358e799fc876
Niveku <kevin.henao@gmail.com>
2015-06-01 20:28:03 -0500

    Creado el primer punto del taller

---
+++ Tareas/arxiv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ figlet "arXiv"
+ echo "----------------------------------------------------------"
+ echo "Buscando en arXiv por nuevo material."
+ echo "Tema: Biología cuantitatíva."
+ echo "Dirección: http://arxiv.org/archive/q-bio/new"
+ echo "Ingrese lo que desea buscar en el material nuevo de esta sección:"
+ read tema
+ echo "=========================================================="
+ echo "El número de artículos nuevos con el tema buscado fue:"
+ respuesta=$(curl -s http://arxiv.org/list/q-bio/new | grep \>Title:| grep -ic $tema)
+ echo $respuesta
+ if [ "$respuesta" != "0" ]; then
+    echo "Los artículos encontrados fueron:"
+    curl -s http://arxiv.org/list/q-bio/new | grep \>Title:| grep -i $tema| sed 's/.*span//g'
+ fi 
