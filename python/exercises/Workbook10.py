8a35733467bc6736d47809def76e68c8b91842e3
KEVIN FELIPE HENAO LOPEZ <kevin.henao@gmail.com>
2015-06-05 11:49:07 -0500

    primer ejercicio de python

---
+++ python/exercises/Workbook10.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[5]:
+
+#Exercise 10:Arithmetic (Solved—20 Lines)
+# Createaprogramthatreadstwointegers, a and b,from the user.Your program should compute and display:
+#• The sum of a and b 
+#• The difference when b is subtracted from a 
+#• The product of a and b
+#• The quotient when a is divided by b
+#• The remainder when a is divided by b
+#• The result of log10a
+#• The result of a^b
+
+from math import exp, expm1
+a = input("valor para a:")
+b = input("valor para b:")
+print a+b
+print a-b
+print a*b
+print a/b
+print a%b
+print math.log10(a)
+print a**b
+
+
+# In[ ]:
+
+
+
